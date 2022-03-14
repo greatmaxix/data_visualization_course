7e8f0f66df389754db34a6245cb318f302947934
greatmaxix <greatmaxix@gmail.com>
2022-03-14 20:30:10 +0600

    lesson 5 copied from teacher

--- lesson5/bar.js
+++ lesson5/bar.js
@@ -7,10 +7,10 @@ async function drawBar() {
         width: width,
         height: width * 0.6,
         margin: {
-            top: 30,
-            right: 10,
-            bottom: 50,
-            left: 50,
+            top: 20,
+            right: 30,
+            bottom: 20,
+            left: 30,
         },
     }
     dimensions.boundedWidth = dimensions.width
@@ -25,23 +25,16 @@ async function drawBar() {
     const wrapper = d3.select("#wrapper")
         .append("svg")
         .attr("width", dimensions.width)
-        .attr("height", dimensions.height)
-        ;
+        .attr("height", dimensions.height);
 
-    const bounds = wrapper.append('g').style(
-        'transform',
-        `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
-    )
-
-    bounds.style('transform', `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+    const bounds = wrapper.append("g")
+        .style("translate", `translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`);
 
     // init static elements
     bounds.append("g")
         .attr("class", "bins")
-
     bounds.append("line")
         .attr("class", "mean")
-
     bounds.append("g")
         .attr("class", "x-axis")
         .style("transform", `translateY(${dimensions.boundedHeight}px)`)
@@ -50,110 +43,91 @@ async function drawBar() {
         .attr("x", dimensions.boundedWidth / 2)
         .attr("y", dimensions.margin.bottom - 10)
 
-    const drawHistogram = (metric) => {
+    const drawHistogram = metric => {
         //Accessor
-        const Accessor = d => d[metric];
+        const metricAccessor = d => d[metric];
         const yAccessor = d => d.length;
 
-        const exitTransition = d3.transition()
-            .duration(600)
-        
-        const updateTransition = exitTransition.transition()
-            .duration(600)
-        
+        const exitTransition = d3.transition().duration(600)
+        const updateTransition = exitTransition.transition().duration(600)
+
         const xScaler = d3.scaleLinear()
-            .domain(d3.extent(dataset, Accessor))
+            .domain(d3.extent(dataset, metricAccessor))
             .range([0, dimensions.boundedWidth])
             .nice()
 
-        const binsGenerator = d3.bin()
+        const binsGen = d3.bin()
             .domain(xScaler.domain())
-            .value(Accessor)
-            .thresholds(12)
+            .value(metricAccessor)
+            .thresholds(12);
 
-        const bins = binsGenerator(dataset)
+        const bins = binsGen(dataset);
+        console.log(bins);
 
         const yScaler = d3.scaleLinear()
             .domain([0, d3.max(bins, yAccessor)])
             .range([dimensions.boundedHeight, 0])
 
-        let binGroups = bounds.select('.bins')
-            .selectAll('.bin')
-            .data(bins)
-        
-        const oldBinGroups = binGroups.exit()
-        oldBinGroups.selectAll('rect')
-            .style('fill', 'orangered')
-            .transition(exitTransition)
-            .attr('y', dimensions.boundedHeight)
-            .attr('height', 0)
+        let binGroups = bounds.select(".bins").selectAll(".bin").data(bins)
 
-        oldBinGroups.selectAll('text')
+        const oldBinGroups = binGroups.exit()
+        oldBinGroups.selectAll("rect")
+            .style("fill", "orangered")
             .transition(exitTransition)
-            .attr('y', dimensions.boundedHeight)
+            .attr("y", dimensions.boundedHeight)
+            .attr('height', 0)
+        oldBinGroups.selectAll("text")
+            .transition(exitTransition)
+            .attr("y", dimensions.boundedHeight)
 
         oldBinGroups.transition(exitTransition).remove()
 
-        const newBinGroups = binGroups.enter().append('g')
-            .attr('class', 'bin')
-        
-        newBinGroups.append('rect')
-        newBinGroups.append('text')
-        newBinGroups.append('mean')
+        const newBinGroups = binGroups.enter().append("g")
+            .attr("class", "bin")
+
+        newBinGroups.append("rect")
+        newBinGroups.append("text")
 
         binGroups = newBinGroups.merge(binGroups)
-        
+
         const barPadding = 1
 
-        const barRect = binGroups.select('rect')
+        const barRect = binGroups.select("rect")
             .transition(updateTransition)
-            .attr('x', d => xScaler(d.x0) + barPadding / 2)
-            .attr('y', d => yScaler(yAccessor(d)))
-            .attr('width', d => d3.max([0, xScaler(d.x1) - xScaler(d.x0) - barPadding]))
-            .attr('height', d => dimensions.boundedHeight - yScaler(yAccessor(d)))
-            .attr('fill', 'steelblue')
+            .attr("x", d => xScaler(d.x0) + barPadding / 2)
+            .attr("y", d => yScaler(yAccessor(d)))
+            .attr("width", d => d3.max([0, xScaler(d.x1) - xScaler(d.x0) - barPadding]))
+            .attr("height", d => dimensions.boundedHeight - yScaler(yAccessor(d)))
             .transition()
-            .style('fill', 'cornflowerblue')
+            .style("fill", "cornflowerblue")
 
-        const barText = binGroups.filter(yAccessor)
-            .select('text')
+
+        const barText = binGroups.select("text")
             .transition(updateTransition)
-            .attr('x', d => xScaler(d.x0) + (xScaler(d.x1) - xScaler(d.x0)) / 2)
-            .attr('y', d => yScaler(yAccessor(d)) - 5)
-            .attr('fill', 'black')
-            .text(d => yAccessor(d) || '')
-            .attr('font-size', '12px')
-            .attr('text-anchor', 'middle')
+            .attr("x", d => xScaler(d.x0) + (xScaler(d.x1) - xScaler(d.x0)) / 2)
+            .attr("y", d => yScaler(yAccessor(d)) - 5)
+            .text(d => yAccessor(d) || "")
 
-        const mean = d3.mean(dataset, Accessor)
 
-        const meanLine = bounds.selectAll('.mean')
+
+        const mean = d3.mean(dataset, metricAccessor);
+        console.log(mean);
+        const meanLine = bounds.selectAll(".mean")
             .transition(updateTransition)
-            .attr('x1', xScaler(mean))
-            .attr('y1', -20)
-            .attr('x2', xScaler(mean))
-            .attr('y2', dimensions.boundedHeight)
-            .attr('stroke', 'black')
-            .attr('stroke-width', 3)
-            .attr('stroke-dasharray', '6px 2px')
+            .attr("x1", xScaler(mean))
+            .attr("x2", xScaler(mean))
+            .attr("y1", -15)
+            .attr("y2", dimensions.boundedHeight)
 
-        // const meanLabel = bounds.append('text')
-        //     .attr('x', xScaler(mean))
-        //     .attr('y', -20)
-        //     .attr('fill', 'black')
-        //     .text(`Mean: ${mean}`)
-        //     .attr('font-size', '12px')
-        //     .attr('text-anchor', 'middle')
-
-        const xAxis = bounds.select('x-axis')
+        const xAxisGen = d3.axisBottom()
+            .scale(xScaler);
+        const xAxis = bounds.select("x-axis")
             .transition(updateTransition)
-            .call(d3.axisBottom(xScaler))
-            .attr('transform', `translate(${dimensions.margin.left}, ${dimensions.margin.top + dimensions.boundedHeight})`)
+            .call(xAxisGen)
+            .style("transform", `translateY(${dimensions.boundedHeight}px)`);
 
-        // const yAxis = bounds.select('y-axis')
-        //     .call(d3.axisLeft(yScaler))
-        //     .attr('transform', `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
     }
+
 
     const metrics = [
         "windSpeed",
@@ -163,20 +137,23 @@ async function drawBar() {
         "uvIndex",
         "windBearing",
         "temperatureMin",
-        "temperatureMax",
+        "temperatureMax"
     ]
-
     let mIndex = 0
+
+    drawHistogram(metrics[mIndex])
     const button = d3.select("body")
         .append("button")
         .text("Change Metric")
 
-    button.node().addEventListener("click", () => {
+    button.node().addEventListener("click", onClick)
+
+    function onClick() {
         mIndex = (mIndex + 1) % metrics.length
         drawHistogram(metrics[mIndex])
-    })
+        console.log(mIndex)
+    }
 
-    drawHistogram(metrics[mIndex])
 }
 
 drawBar()
